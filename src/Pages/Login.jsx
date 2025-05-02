3e71a55e9e09729bad8541e1f0dabaff42aa7574
Suprotik007 <csuprotik@gmail.com>
2025-05-02 16:12:19 +0600

    login function

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { use } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../Provider/AuthProvider';
 
 const Login = () => {
+  const {signIn}=use(AuthContext)
+  const handleLogin=(e)=>{
+    e.preventDefault()
+    console.log(e.target);
+  
+    const email=e.target.email.value
+    const password=e.target.password.value
+    console.log({email,password})
+    signIn(email,password)
+    .then((result)=>{
+      const user=result.user
+      console.log(user);
+      
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorCode,errorMessage)
+    });
+  }
+
+
     return (
         <div>
              
@@ -10,16 +33,16 @@ const Login = () => {
     <div className="card bg-base-100 p-5 rounded-2xl w-full max-w-sm shrink-0 shadow-2xl">
       <div className="card-body">
       <h1 className="text-4xl text-accent mb-10 font-bold text-center">Login now!</h1>
-        <fieldset className="fieldset">
+        <form onSubmit={handleLogin} className="fieldset">
           <label className="label font-semibold">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input type="email" name='email' className="input" placeholder="Email" />
           <label className="label font-semibold">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input type="password" name='password' className="input" placeholder="Password" />
           <div><a className="link link-hover text-accent font-semibold">Forgot password?</a></div>
-          <button className="btn btn-neutral mt-4">Login</button>
+          <button type='submit' className="btn btn-neutral mt-4">Login</button>
 
           <p className='text-accent font-semibold text-center mt-3'>Dont’t Have An Account ? <Link to='/auth/reg'> <span className='text-orange-700 font-semibold'>Register</span></Link></p>
-        </fieldset>
+        </form>
       </div>
     </div>
   </div>
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useEffect, useState } from 'react';
 import app from '../Firebase';
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 const auth = getAuth(app);
 export const AuthContext=createContext();
 const AuthProvider = ({children}) => {
@@ -8,6 +8,10 @@ const AuthProvider = ({children}) => {
 
 const createUser=(email,password)=>{
     return createUserWithEmailAndPassword(auth,email,password)
+}
+
+const signIn=(email,password)=>{
+    return signInWithEmailAndPassword(auth,email,password)
 }
 const logOut=()=>{
 return signOut(auth)
@@ -26,7 +30,8 @@ useEffect(()=>{
         user,
         setUser,
         createUser,
-        logOut
+        logOut,
+        signIn
     };
     return <AuthContext value={authData}>{children}</AuthContext>;
         
